3dc9512201f6e1d9fdc41e901f4e7882f2040be0
Michael Rivnak <mrivnak>
2018-11-06 18:35:32 -0500

    began button handlers

--- src/rentr.java
+++ src/rentr.java
@@ -6,7 +6,14 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class rentr {
+    ArrayList<Rental> likedRentals = new ArrayList<>();
+    ArrayList<Purchase> likedPurchases = new ArrayList<>();
+    ArrayList<Rental> seenRentals = new ArrayList<>();
+    ArrayList<Purchase> seenPurchases = new ArrayList<>();
+
     public void start(Stage primaryStage) throws Exception {
         Button like = new Button("Like");
         Button pass = new Button("Pass");
@@ -19,16 +26,40 @@ public class rentr {
     }
 
     class likeHandler implements EventHandler<ActionEvent> {
+        private Rental tempRental;
+        private Purchase tempPurchase;
+        public void setCurrentProperty(Rental rental) {
+            tempRental = rental;
+        }
+
+        public void setCurrentProperty(Purchase purchase) {
+            tempPurchase = purchase;
+        }
+
+        public void addCurrentProperty() {
+            if (tempRental != null) {
+                likedRentals.add(tempRental);
+                seenRentals.add(tempRental);
+                tempRental = null;
+            } else if (tempPurchase != null) {
+                likedPurchases.add(tempPurchase);
+                seenPurchases.add(tempPurchase);
+                tempPurchase = null;
+            }
+        }
+
         @Override
         public void handle(ActionEvent event) {
-
+            addCurrentProperty();
         }
     }
 
     class passHandler implements EventHandler<ActionEvent> {
         @Override
         public void handle(ActionEvent event) {
-
+        /*
+        Adds that property to the "seen" list
+         */
         }
     }
 
